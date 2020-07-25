f46d0cdfab8fcd27a271c43b4bc5666219be9713
Fabio Gallo <fabiorafael22@gmail.com>
2020-07-25 19:03:36 -0300

    New book chapter (3).

--- pruebas01/pruebas.py
+++ pruebas01/pruebas.py
@@ -14,6 +14,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+show_graphics = False
 print("Hello")
 
 bank_train = pd.read_csv("../datasets/bank_marketing_training") # pag 12
@@ -40,7 +42,8 @@ print(bank_train[['age','job']]) # pag 15
 
 print("_______\n Gráficos") # pag 17, es necesario añadir la libreria matplotlib
 bank_train['age'].plot(kind='hist')
-plt.show()
+if show_graphics:
+    plt.show()
 
 """
     Capítulo 3
@@ -53,7 +56,33 @@ bank_train['index'] = pd.Series(range(0,26874)) # pag 32
 print("Head: ", bank_train.head) # pag 32
 
 # 3.5.1 how to Change Misleading Field Values Using python, Pag 34
-print("\n Before replacement of  999 for NaN:\n", bank_train['days_since_previous'])
+#print("\n Before replacement of  999 for NaN:\n", bank_train['days_since_previous'])
+bank_train['days_since_previous'].plot(kind = 'hist',title = 'Histogram of Days Since Previous (before replacement 999 for NaN)')
+if show_graphics:
+    plt.show()
+
 bank_train['days_since_previous'] = bank_train['days_since_previous'].replace({999: np.NaN})
-print("\n After replacement of 999 for NaN:\n", bank_train['days_since_previous'])
+
+#print("\n After replacement of 999 for NaN:\n", bank_train['days_since_previous'])
+bank_train['days_since_previous'].plot(kind = 'hist',title = 'Histogram of Days Since Previous (before replacement 999 for NaN)')
+if show_graphics:
+    plt.show()
+
+# 3.6.1 how to reexpress Categorical Field Values Using python. Pag 36
+bank_train['education_numeric'] = bank_train['education']
+print("Head after adding column 'education_numeric': ", bank_train.head)
+print("\n 'education_numeric' BEFORE using dictionary: ", bank_train['education_numeric'])
+dict_edu = {"education_numeric":
+                {"illiterate": 0,
+                 "basic.4y": 4,
+                 "basic.6y": 6,
+                 "basic.9y": 9,
+                 "high.school": 12,
+                 "professional.course": 12,
+                 "university.degree": 16,
+                 "unknown": np.NaN}}
+bank_train.replace(dict_edu, inplace=True) # pag 38
+print("\n 'education_numeric' AFTER using dictionary: ", bank_train['education_numeric'])
+
 print("_______\n ")
+
